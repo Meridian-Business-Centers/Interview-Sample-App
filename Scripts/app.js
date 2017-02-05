20d3e7c4fff0176740ca0487cadb33fd0b866689
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-06 01:31:27 +0200

    implement app.js

--- Scripts/app.js
+++ Scripts/app.js
@@ -1,1 +1,17 @@
-. 
+(function () {
+    var app = angular.module('app', ['ngRoute']);
+
+    app.config(function ($routeProvider) { 
+        $routeProvider.when('/home',{
+            controller:'mainController',
+            templateUrl:'/Views/home.html'
+        });
+
+        $routeProvider.when('/detail',{
+            controller:'detailController',
+            templateUrl:'/Views/detail.html'
+        });
+
+        $routeProvider.otherwise({ redirectTo: '/home' });
+    });
+}());
