54da258efe44b5f06609d66f01ec1064fd72a343
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-07 01:30:49 +0200

    clear dump code

--- Scripts/mainController.js
+++ Scripts/mainController.js
@@ -4,19 +4,7 @@
     app.controller('mainController', function ($scope, weatherService, $location) {
         // to do : 
         // - get zipcodes via weatherService after load
-        // - append zipcode buttons
-
-        function generateZipButtons(zipArray) {
-            for (var i = 0; i < zipArray.length; i++) {
-                /* var button = $('<input/>').attr({
-                     type: "button",
-                     id: "zip-btn" + i,
-                     value: zipArray[i]
-                 });*/
-                var button = generateElement(zipArray[i], "zip-btn" + i, "btnClick");
-                $("div.container").append(button);
-            }
-        };
+        // - ng-generate zipcode buttons
 
         function init() {
             weatherService.getZipcodesOfDallas().then(function (response) {
@@ -29,15 +17,6 @@
         $scope.goToDetail = function (zip) {
             $location.path('/detail/' + zip);
         };
-
-
-        function generateElement(value, id, onclickEvent) {
-            return '<input type="button" data-id=' + id + ' value="' + value + '" class="zip-btn" />';
-
-            /*return '<input type="button" data-id=' + id + ' value="' + value + '" onclick="' + onclickEvent + '()" class="btn btn-default" />';*/
-        };
-
-
 
         init();
     });
