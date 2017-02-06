af8c4bbb72aa839c34deb2a361727f4096190559
ahmetgdy <ahmetgokdayi@outlook.com>
2017-02-06 06:31:02 +0200

    implement restify data service

---
+++ server-app/dataService.js
@@ -0,0 +1,21 @@
+var restify = require('restify');
+var fs = require('fs');
+
+function respond(req, res, next) {
+    res.send('hello ' + req.params.name);
+    next();
+};
+
+function getAllZipcodes(req, res, next) {
+    var m = JSON.parse(fs.readFileSync('data.json').toString());
+    res.send(m);
+};
+
+var server = restify.createServer();
+server.get('/hello/:name', respond);
+server.head('/hello/:name', respond);
+server.get('/zip', getAllZipcodes)
+
+server.listen(8080, function () {
+    console.log('%s listening at %s', server.name, server.url);
+});
